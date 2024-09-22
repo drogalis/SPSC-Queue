0f00d7a7a0fa5e4396caad0599d1f41a17ce10d5
Andrew Drogalis <108765079+drogalis@users.noreply.github.com>
2024-09-22 18:29:56 -0400

    Full queue benchmark comparison

--- benchmarks/spsc-queue-benchmark.cpp
+++ benchmarks/spsc-queue-benchmark.cpp
@@ -3,10 +3,28 @@
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
+#include <numeric>
 #include <pthread.h>
 #include <sched.h>
 #include <stdexcept>
 #include <thread>
+#include <vector>
+
+#if __has_include(<rigtorp/SPSCQueue.h> )
+#include <rigtorp/SPSCQueue.h>
+#endif
+
+#if __has_include(<boost/lockfree/spsc_queue.hpp> )
+#include <boost/lockfree/spsc_queue.hpp>
+#endif
+
+#if __has_include(<folly/ProducerConsumerQueue.h>)
+#include <folly/ProducerConsumerQueue.h>
+#endif
+
+#if __has_include(<readerwriterqueue/readerwriterqueue.h>)
+#include <readerwriterqueue/readerwriterqueue.h>
+#endif
 
 void pinThread(int cpu)
 {
@@ -43,69 +61,401 @@ int main(int argc, char** argv)
     TestSize(int x) : x_(x) {}
   };
 
+  const std::size_t trialSize {11};
+  static_assert(trialSize % 2, "Trial size must be odd");
+
   const std::size_t queueSize {10'000'000};
   const std::size_t iters {10'000'000};
+  std::vector<std::size_t> operations(trialSize);
+  std::vector<std::size_t> roundTripTime(trialSize);
 
   std::cout << "Dro SPSC_Queue: \n";
 
+  for (int i {}; i < trialSize; ++i)
   {
-    dro::SPSC_Queue<TestSize> queue(queueSize);
-    auto thrd = std::thread([&]() {
-      pinThread(cpu1);
-      for (int i {}; i < iters; ++i)
-      {
-        while (! queue.front()) {}
-        if ((*queue.front()).x_ != i)
-        {
-          throw std::runtime_error("Value not equal");
-        }
-        queue.try_pop();
-      }
-    });
-
-    pinThread(cpu2);
-
-    auto start = std::chrono::steady_clock::now();
-    for (int i {}; i < iters; ++i) { queue.emplace(TestSize(i)); }
-    thrd.join();
-    auto stop = std::chrono::steady_clock::now();
-
-    std::cout << iters * 1'000'000 /
-                     std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
-                                                                          start)
-                         .count()
-              << " ops/ms \n";
-  }
-
-  {
-    dro::SPSC_Queue<TestSize> q1(queueSize), q2(queueSize);
-    auto thrd = std::thread([&]() {
-      pinThread(cpu1);
-      for (int i {}; i < iters; ++i)
-      {
-        while (! q1.front()) {}
-        q2.emplace(*q1.front());
-        q1.try_pop();
-      }
-    });
-
-    pinThread(cpu2);
-
-    auto start = std::chrono::steady_clock::now();
-    for (int i {}; i < iters; ++i)
     {
-      q1.emplace(TestSize(i));
-      while (! q2.front()) {}
-      q2.try_pop();
+      dro::SPSC_Queue<TestSize> queue(queueSize);
+      auto thrd = std::thread([&]() {
+        pinThread(cpu1);
+        for (int i {}; i < iters; ++i)
+        {
+          while (! queue.front()) {}
+          if ((*queue.front()).x_ != i)
+          {
+            throw std::runtime_error("Value not equal");
+          }
+          queue.try_pop();
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i {}; i < iters; ++i) { queue.emplace(TestSize(i)); }
+      thrd.join();
+      auto stop = std::chrono::steady_clock::now();
+
+      operations[i] =
+          iters * 1'000'000 /
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count();
     }
-    auto stop = std::chrono::steady_clock::now();
-    thrd.join();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
-                                                                      start)
-                         .count() /
-                     iters
-              << "ns RTT \n";
+
+    {
+      dro::SPSC_Queue<TestSize> q1(queueSize), q2(queueSize);
+      auto thrd = std::thread([&]() {
+        pinThread(cpu1);
+        for (int i {}; i < iters; ++i)
+        {
+          while (! q1.front()) {}
+          q2.emplace(*q1.front());
+          q1.try_pop();
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i {}; i < iters; ++i)
+      {
+        q1.emplace(TestSize(i));
+        while (! q2.front()) {}
+        q2.try_pop();
+      }
+      auto stop = std::chrono::steady_clock::now();
+      thrd.join();
+      roundTripTime[i] =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count() /
+          iters;
+    }
   }
+
+  std::sort(operations.begin(), operations.end());
+  std::sort(roundTripTime.begin(), roundTripTime.end());
+  std::cout << "Mean: "
+            << std::accumulate(operations.begin(), operations.end(), 0) /
+                   trialSize
+            << " ops/ms \n";
+  std::cout << "Median: " << operations[trialSize / 2] << " ops/ms \n";
+
+  std::cout << "Mean: "
+            << std::accumulate(roundTripTime.begin(), roundTripTime.end(), 0) /
+                   trialSize
+            << " ns RTT \n";
+  std::cout << "Median: " << roundTripTime[trialSize / 2] << " ns RTT \n";
+
+#if __has_include(<rigtorp/SPSCQueue.h> )
+
+  std::cout << "Rigtorp SPSCQueue:" << std::endl;
+
+  for (int i {}; i < trialSize; ++i)
+  {
+    {
+      rigtorp::SPSCQueue<TestSize> q(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          while (! q.front());
+          if ((*q.front()).x_ != i)
+          {
+            throw std::runtime_error("");
+          }
+          q.pop();
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i) { q.emplace(TestSize(i)); }
+      t.join();
+      auto stop = std::chrono::steady_clock::now();
+      operations[i] =
+          iters * 1000000 /
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count();
+    }
+
+    {
+      rigtorp::SPSCQueue<TestSize> q1(queueSize), q2(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          while (! q1.front());
+          q2.emplace(*q1.front());
+          q1.pop();
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i)
+      {
+        q1.emplace(TestSize(i));
+        while (! q2.front());
+        q2.pop();
+      }
+      auto stop = std::chrono::steady_clock::now();
+      t.join();
+      roundTripTime[i] =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count() /
+          iters;
+    }
+  }
+  std::sort(operations.begin(), operations.end());
+  std::sort(roundTripTime.begin(), roundTripTime.end());
+  std::cout << "Mean: "
+            << std::accumulate(operations.begin(), operations.end(), 0) /
+                   trialSize
+            << " ops/ms \n";
+
+  std::cout << "Median: " << operations[trialSize / 2] << " ops/ms \n";
+  std::cout << "Mean: "
+            << std::accumulate(roundTripTime.begin(), roundTripTime.end(), 0) /
+                   trialSize
+            << " ns RTT \n";
+  std::cout << "Median: " << roundTripTime[trialSize / 2] << " ns RTT \n";
+
+#endif
+
+#if __has_include(<boost/lockfree/spsc_queue.hpp> )
+  std::cout << "boost::lockfree::spsc:" << std::endl;
+  for (int i {}; i < trialSize; ++i)
+  {
+    {
+      boost::lockfree::spsc_queue<TestSize> q(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          TestSize val;
+          while (q.pop(&val, 1) != 1);
+          if (val.x_ != i)
+          {
+            throw std::runtime_error("");
+          }
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i) { while (! q.push(TestSize(i))); }
+      t.join();
+      auto stop = std::chrono::steady_clock::now();
+      operations[i] =
+          iters * 1000000 /
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count();
+    }
+
+    {
+      boost::lockfree::spsc_queue<TestSize> q1(queueSize), q2(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          TestSize val;
+          while (q1.pop(&val, 1) != 1);
+          while (! q2.push(val));
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i)
+      {
+        while (! q1.push(TestSize(i)));
+        TestSize val;
+        while (q2.pop(&val, 1) != 1);
+      }
+      auto stop = std::chrono::steady_clock::now();
+      t.join();
+      roundTripTime[i] =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count() /
+          iters;
+    }
+  }
+
+  std::sort(operations.begin(), operations.end());
+  std::sort(roundTripTime.begin(), roundTripTime.end());
+  std::cout << "Mean: "
+            << std::accumulate(operations.begin(), operations.end(), 0) /
+                   trialSize
+            << " ops/ms \n";
+  std::cout << "Median: " << operations[trialSize / 2] << " ops/ms \n";
+
+  std::cout << "Mean: "
+            << std::accumulate(roundTripTime.begin(), roundTripTime.end(), 0) /
+                   trialSize
+            << " ns RTT \n";
+  std::cout << "Median: " << roundTripTime[trialSize / 2] << " ns RTT \n";
+
+#endif
+
+#if __has_include(<folly/ProducerConsumerQueue.h>)
+  std::cout << "folly::ProducerConsumerQueue:" << std::endl;
+  for (int i {}; i < trialSize; ++i)
+  {
+    {
+      folly::ProducerConsumerQueue<TestSize> q(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          TestSize val;
+          while (! q.read(val));
+          if (val.x_ != i)
+          {
+            throw std::runtime_error("");
+          }
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i) { while (! q.write(TestSize(i))); }
+      t.join();
+      auto stop = std::chrono::steady_clock::now();
+      operations[i] =
+          iters * 1000000 /
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count();
+    }
+
+    {
+      folly::ProducerConsumerQueue<TestSize> q1(queueSize), q2(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          TestSize val;
+          while (! q1.read(val));
+          q2.write(val);
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i)
+      {
+        while (! q1.write(TestSize(i)));
+        TestSize val;
+        while (! q2.read(val));
+      }
+      auto stop = std::chrono::steady_clock::now();
+      t.join();
+      roundTripTime[i] =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count() /
+          iters;
+    }
+  }
+
+  std::sort(operations.begin(), operations.end());
+  std::sort(roundTripTime.begin(), roundTripTime.end());
+  std::cout << "Mean: "
+            << std::accumulate(operations.begin(), operations.end(), 0) /
+                   trialSize
+            << " ops/ms \n";
+  std::cout << "Median: " << operations[trialSize / 2] << " ops/ms \n";
+
+  std::cout << "Mean: "
+            << std::accumulate(roundTripTime.begin(), roundTripTime.end(), 0) /
+                   trialSize
+            << " ns RTT \n";
+  std::cout << "Median: " << roundTripTime[trialSize / 2] << " ns RTT \n";
+
+#endif
+
+#if __has_include(<readerwriterqueue/readerwriterqueue.h>)
+  std::cout << "moodycamel::ReaderWriterQueue" << std::endl;
+  for (int i {}; i < trialSize; ++i)
+  {
+    {
+      moodycamel::ReaderWriterQueue<TestSize> q(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          while (! q.peek());
+          if ((*q.peek()).x_ != i)
+          {
+            throw std::runtime_error("");
+          }
+          TestSize val {};
+          q.try_dequeue(val);
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i) { q.try_enqueue(TestSize(i)); }
+      t.join();
+      auto stop = std::chrono::steady_clock::now();
+      operations[i] =
+          iters * 1000000 /
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count();
+    }
+
+    {
+      moodycamel::ReaderWriterQueue<TestSize> q1(queueSize), q2(queueSize);
+      auto t = std::thread([&] {
+        pinThread(cpu1);
+        for (int i = 0; i < iters; ++i)
+        {
+          while (! q1.peek());
+          q2.try_enqueue(*q1.peek());
+          TestSize val {};
+          q1.try_dequeue(val);
+        }
+      });
+
+      pinThread(cpu2);
+
+      auto start = std::chrono::steady_clock::now();
+      for (int i = 0; i < iters; ++i)
+      {
+        q1.try_enqueue(TestSize(i));
+        while (! q2.peek());
+        TestSize val {};
+        q2.try_dequeue(val);
+      }
+      auto stop = std::chrono::steady_clock::now();
+      t.join();
+      roundTripTime[i] =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start)
+              .count() /
+          iters;
+    }
+  }
+
+  std::sort(operations.begin(), operations.end());
+  std::sort(roundTripTime.begin(), roundTripTime.end());
+  std::cout << "Mean: "
+            << std::accumulate(operations.begin(), operations.end(), 0) /
+                   trialSize
+            << " ops/ms \n";
+  std::cout << "Median: " << operations[trialSize / 2] << " ops/ms \n";
+
+  std::cout << "Mean: "
+            << std::accumulate(roundTripTime.begin(), roundTripTime.end(), 0) /
+                   trialSize
+            << " ns RTT \n";
+  std::cout << "Median: " << roundTripTime[trialSize / 2] << " ns RTT \n";
+
+#endif
 
   return 0;
 }
